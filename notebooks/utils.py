afda30dc873162181d13eca747dfde9ff16746f1
mcao610 <c_meng@outlook.com>
2021-01-26 10:13:11 -0500

    finish Maynez experiments

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,3 +1,4 @@
+import os
 import json
 
 
@@ -130,3 +131,34 @@ def prepare_cmlm_inputs(source, target, ent_parts=None):
         positions.append((e['start'] + 4, e['end'] + 4))
 
     return inputs, targets, positions, entities
+
+
+def process_document(raw_doc):
+    TRIVIAL_SENTS = [
+        'Share this with',
+        'Copy this link',
+        'These are external links and will open in a new window',
+    ]
+    
+    raw_doc = raw_doc.strip()
+    raw_doc_sents = raw_doc.split('\n')
+    
+    start_signal = False
+    filtered_sentences = []
+    for s in raw_doc_sents: 
+        if start_signal:
+            filtered_sentences.append(s)
+        elif len(s.split()) > 1 and s not in TRIVIAL_SENTS:
+            start_signal = True
+            filtered_sentences.append(s)
+            
+    return ' '.join(filtered_sentences)
+
+
+def read_document(bbcid, folder):
+    file_path = folder + '{}.document'.format(bbcid)
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            return process_document(f.read())
+    else:
+        return None
