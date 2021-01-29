75b068e3ce53d78b54d246b5a3b00a7ad90c79fb
mcao610 <c_meng@outlook.com>
2021-01-28 21:37:11 -0500

    add histgram

--- notebooks/draw.py
+++ notebooks/draw.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from sklearn import neighbors
 from sklearn.metrics import roc_curve, auc
+from matplotlib.colors import ListedColormap
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
@@ -105,6 +106,84 @@ def plot(taskname,
     plt.show()
 
 
+def plot_three_with_boundary(taskname, 
+                             priors, 
+                             posteriors, 
+                             labels, 
+                             n_neighbors=15, 
+                             fig_size=(4.5, 3.5), 
+                             colors=['red', 'blue'], 
+                             legend_labels=['Non-factual', 'Factual'],
+                             x_label='Prior Probability',
+                             y_label='Posterior Probability',
+                             save_figure=False,
+                             interval=0.2,
+                             h=0.02):
+    """Draw KNN classification boundaries."""
+    classifier = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm='brute')
+
+    larray = np.array(labels)
+    priors = np.array(priors)
+    posteriors = np.array(posteriors)
+    
+    pin = posteriors
+    x_mat = np.vstack([pin / np.std(pin), priors / np.std(priors)]).transpose()
+    y_vec = np.array(labels)
+    
+    classifier.fit(x_mat, y_vec)
+    
+    # Plot the decision boundary. For that, we will asign a color to each
+    # point in the mesh [x_min, m_max]x[y_min, y_max].
+    x_min, x_max = x_mat[:, 0].min() - .5, x_mat[:, 0].max() + .5
+    y_min, y_max = x_mat[:, 1].min() - .5, x_mat[:, 1].max() + .5
+    
+    h = 0.02
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = classifier.predict_proba(np.c_[xx.ravel(), yy.ravel()])
+    Z_prediction = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape((xx.shape[0], xx.shape[1], 3))  # [80, 72, 3]
+    Z_prediction = Z_prediction.reshape(xx.shape)
+
+    xx_1, yy_1 = np.meshgrid(np.arange(x_min, x_max, h) * np.std(pin),
+                             np.arange(y_min, y_max, h) * np.std(priors))
+    
+    # Plot color map
+    fig, ax = plt.subplots(figsize=fig_size)
+    z_max_index = np.argmax(Z, axis=2)
+    nb_classes = 3
+    one_hot_max_index = np.eye(nb_classes)[z_max_index.reshape(-1)]
+    one_hot_max_index = one_hot_max_index.reshape(Z.shape)
+    Z_c = Z * one_hot_max_index
+    Z_c = Z_c + (one_hot_max_index - 1)
+
+    ax.contourf(xx_1, yy_1, Z_c[:, :, 0], cmap=plt.cm.Blues, alpha=.5, levels=np.arange(0.0, 1.1, interval))
+    ax.contourf(xx_1, yy_1, Z_c[:, :, 1], cmap=plt.cm.Greens, alpha=.7, levels=np.arange(0.0, 1.1, interval))
+    ax.contourf(xx_1, yy_1, Z_c[:, :, 2], cmap=plt.cm.Reds, alpha=.5, levels=np.arange(0.0, 1.1, interval))
+
+#     cmap_light = ListedColormap(['#AAFFAA', '#AAAAFF', '#FFAAAA'])
+#     ax.pcolormesh(xx_1, yy_1, Z_prediction, cmap=cmap_light, alpha=.8)
+
+    ax.scatter(np.array(pin)[np.nonzero(larray==0)[0]], np.array(priors)[np.nonzero(larray==0)[0]], color=colors[0], 
+               edgecolor='black', label=legend_labels[0], marker='s', alpha=0.8)
+    ax.scatter(np.array(pin)[np.nonzero(larray==1)[0]], np.array(priors)[np.nonzero(larray==1)[0]], color=colors[1], 
+               edgecolor='black', label=legend_labels[1], alpha=0.8)
+    ax.scatter(np.array(pin)[np.nonzero(larray==2)[0]], np.array(priors)[np.nonzero(larray==2)[0]], color=colors[2], 
+               edgecolor='black', label=legend_labels[2], marker="D", alpha=0.8)
+
+    ax.legend(loc='lower right')
+    ax.set_xlim(np.min(xx_1), np.max(xx_1))
+    ax.set_ylim(np.min(yy_1), np.max(yy_1))
+    ax.set_xlabel(x_label, fontweight ='bold', fontsize=11)
+    ax.set_ylabel(y_label, fontweight ='bold', fontsize=11)
+#     axHistx.set_title(taskname)
+    
+    plt.tight_layout()
+    if save_figure:
+        plt.savefig("figures/" + taskname +'.pdf')
+    plt.show()
+
+
 def plot_three(taskname, 
                priors, 
                posteriors, 
@@ -132,7 +211,7 @@ def plot_three(taskname,
     ax.scatter(np.array(pin)[np.nonzero(larray==1)[0]], np.array(priors)[np.nonzero(larray==1)[0]], color=colors[1], 
                edgecolor='black', label=legend_labels[1], alpha=0.8)
     ax.scatter(np.array(pin)[np.nonzero(larray==2)[0]], np.array(priors)[np.nonzero(larray==2)[0]], color=colors[2], 
-               edgecolor='black', label=legend_labels[2], alpha=0.8)
+               edgecolor='black', label=legend_labels[2], marker="D", alpha=0.8)
     
     ax.legend(loc='lower right')
     ax.set_xlabel(x_label, fontweight='bold', fontsize=11)
@@ -164,4 +243,37 @@ def draw_auc(target, probs, labels, colors, fig_name=None):
     plt.legend(loc="lower right")
     if fig_name is not None:
         plt.savefig('figures/{}.pdf'.format(fig_name))
+    plt.show()
+
+
+def plot_hist(taskname, posteriors, priors, save_fig=True):
+    n_bins = 10
+    fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2, figsize=(7.5, 3.5))
+    
+    _, bins, _ = ax0.hist(posteriors[0], n_bins, density=True, histtype='bar', color='blue', label='Non-hallucinated', edgecolor='blue', alpha=0.55)
+    ax0.hist(posteriors[1], bins=bins + 0.13, density=True, histtype='bar', color='darkgreen', label='Factual hallucination', edgecolor='darkgreen', alpha=0.6)
+    ax0.set_ylim([0., 1.1])
+    ax0.legend(prop={'size': 11})
+    ax0.set_ylabel('CMLM', fontsize=12)
+    
+    _, bins, _ = ax1.hist(posteriors[0], n_bins, density=True, histtype='bar', color='blue', label='Non-hallucinated', edgecolor='blue', alpha=0.55)
+    ax1.hist(posteriors[2], bins=bins + 0.12, density=True, histtype='bar', color='red', label='Non-factual Hallucination', edgecolor='red', alpha=0.55)
+    ax1.set_ylim([0., 1.1])
+    ax1.legend(prop={'size': 11})
+    ax1.set_yticklabels([])
+    
+    _, bins, _ = ax2.hist(priors[0], n_bins, density=True, histtype='bar', color='blue', label='Non-hallucinated', edgecolor='blue', alpha=0.55)
+    ax2.hist(priors[1], bins=bins + 0.25, density=True, histtype='bar', color='darkgreen', label='Factual hallucination', edgecolor='darkgreen', alpha=0.6)
+    ax2.set_ylabel('MLM', fontsize=12)
+    
+    # Make a multiple-histogram of data-sets with different length.
+    _, bins, _ = ax3.hist(priors[0], n_bins, density=True, histtype='bar', color='blue', label='Non-hallucinated', edgecolor='blue', alpha=0.55)
+    ax3.hist(priors[2], bins=bins + 0.26, density=True, histtype='bar', color='red', label='Non-factual hallucination', edgecolor='red', alpha=0.55)
+    ax3.set_yticklabels([])
+    
+    fig.text(0.5, -0.005, 'Prediction Entropy', ha='center', fontsize=12)
+    fig.text(0.99, 0.5, 'Density', va='center', rotation='vertical', fontsize=12)
+    fig.tight_layout()
+    if save_fig:
+        plt.savefig("figures/" + taskname +'.pdf', bbox_inches="tight")
     plt.show()
