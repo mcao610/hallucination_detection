d1c9e4d4f5df146db1fec03b2030207b22548f9c
mcao610 <c_meng@outlook.com>
2021-03-01 21:45:14 -0500

    delete gitignore

--- jupyter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-source ~/env37/bin/activate
-
-CUSTOM_PORT=8996
-ssh -f -N -R $CUSTOM_PORT:localhost:$CUSTOM_PORT beluga2
-export JUPYTER_RUNTIME_DIR=$SLURM_TMPDIR
-jupyter lab --port=$CUSTOM_PORT --no-browser
